1ac23c65a1db6284033e8d127d601970f0d8c57d
kevinvalen12 <valenciakevin894@gmail.com>
2026-06-13 15:56:03 -0500

    ejercicios de boolean primitivos

---
+++ src/PrimitivosBoolean.java
@@ -0,0 +1,17 @@
+public class PrimitivosBoolean {
+    public static void main(String[] args) {
+
+        boolean datoLogico = true;
+        System.out.println("boolean: " + datoLogico);
+
+        double d = 98765.43E-3;
+        float f = 1.2345E2F;
+        
+        datoLogico = (d > f);
+        System.out.println(datoLogico);
+
+        boolean esIgual = ((3-2) == 1);
+        System.out.println(esIgual);
+      }
+}
+ 
