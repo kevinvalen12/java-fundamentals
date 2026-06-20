4b0aab0474e2e4bb3f021f08494b5948ab6dbfc5
kevinvalen12 <valenciakevin894@gmail.com>
2026-06-20 08:51:58 -0500

    validar numeros pares

--- src/OperadorAritmetico.java
+++ src/OperadorAritmetico.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class OperadorAritmetico {
     public int i; 
     public int j;
@@ -8,26 +10,66 @@ public class OperadorAritmetico {
     }
 
     public int sumaTtotal(int i, int j) {
-
         int resultado = i + j;
         return resultado;
     }
 
     public int restaTotal(int i, int j) {
-        int resulatdo = i -j;
+        int resulatdo =  i - j;
         return resulatdo;
     }
 
+    public int multiplicacionTotal(int i, int j) {
+        int resulatdo = i * j;
+        return resulatdo;
+    }
+
+    public float divisionesTotal(int i, int j) {
+        float floatI = (float)i;
+        float floatJ = (float)j;
+        float resulatdo = floatI / floatJ;
+        return resulatdo;
+    }
+
+    public int restoTotal(int sumaTtotal, int j) {
+        int resultado = i % j;
+        return resultado;
+    }
+
+    public void validarNumeroPrimo(int valroDIgital, int resto) {
+        boolean restoIf = resto == 0;
+
+        if (!restoIf) {
+            System.out.println(valroDIgital + " no es un numero primo");
+
+        } else {
+            System.out.println(valroDIgital + " es un numero primo");
+        }
+    }
+
     public static void main(String[] args) {
-        int i = 4, j = 6;
+        int i = 56, j = 6;
         
         OperadorAritmetico valor = new OperadorAritmetico(i, j);
         
         int suma = valor.sumaTtotal(45, 5);
-        System.out.println("resultado de la suma" + suma);
+        System.out.println("resultado de la suma: " + suma);
 
         int resta = valor.sumaTtotal(i, j);
-        System.out.println("resultado de la resta" + resta);
+        System.out.println("resultado de la resta: " + resta);
+
+        int multiplicacion = valor.multiplicacionTotal(i, j);
+        System.out.println("resultado de la multilicación: " + multiplicacion);
+
+        float division = valor.divisionesTotal(i, j);
+        System.out.println("resultado de laa division: " + division);
+
+        int valorDigital = Integer.parseInt(JOptionPane.showInputDialog("escribe el valor"));
+        int resto = valor.restoTotal(valorDigital, j);
+        System.out.println("resultado de laa reesto de la división: " + resto);
+        
+
+        valor.validarNumeroPrimo(valorDigital, resto);
 
     }
     
