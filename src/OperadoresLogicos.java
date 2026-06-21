49329e991b0b3b6211e3a1a8edf595c300b30e4a
kevinvalen12 <valenciakevin894@gmail.com>
2026-06-21 11:30:39 -0500

    Ejercicios de operadores logicos

--- src/OperadoresLogicos.java
+++ src/OperadoresLogicos.java
@@ -20,21 +20,47 @@ public class OperadoresLogicos {
 
     }
 
-    public boolean relacionDeIgualdad() {
-        boolean resulatdo = i == j && k > l;
+    public boolean operadorAnd() {
+        boolean resulatdo = i == j && k < l && !m;
         return resulatdo;
     }
 
-    
+    public boolean operadorOr() {
+        boolean resulatdo = i == j || k < l;
+        return resulatdo;
+    }
 
+    public boolean operadorAndOr() {
+        boolean resultado = i == j && (k > l || !m);
+        return resultado; 
+    }
+
+    public boolean bCuatro() {
+        /*
+            TODO: en este la mayoria de veces el codigo se ejecuta de izquierda a derecho
+            TODO: como el AND(&&) tiene prioridad que el OR(||) primeor se ejecua
+                            * k < l && !m  ! imverte el valor ya no seria false sino true 
+            ? y el resultado para a hacer la comparacion con
+                            * i == j ||
+
+        */
+        boolean resulatdo = i == j || k < l && !m;
+        return resulatdo;
+    }
+
+    /*
+        TODO: siempre va a tener prioridad el AND(&&) antes que OR(||)
+    */
     public void mostrarResulatdos() {
-        System.out.println("i == j && k > j: " + relacionDeIgualdad());
-        
+        System.out.println("i == j && k < j && l && m == true: " + operadorAnd());
+        System.out.println("i == j || k > l: " + operadorOr());
+        System.out.println("i == j && k > l || m: " + operadorAndOr());
+        System.out.println("i == j && k > l || m: " + bCuatro());
     }
 
     public static void main(String[] args) {
         int i = 3;
-        byte j = 7;
+        byte j = 3;
         float k = 127E-7f;
         double l = 2.1413E3;
         boolean m = false;
